311b4b4017938eb78928f8a43eb8430f9bdef607
guoxuemin <guo469477762@gmail.com>
2016-02-05 22:21:46 +0800

    add

---
+++ index.js
@@ -0,0 +1,124 @@
+var projectPath = fis.project.getProjectPath();
+
+// 获取项目名称
+var project = require(projectPath+'/package.json').name;
+
+// 输出路径
+var outputPath = "../../output";
+
+//匹配组件标签<w-widget></w-widget>
+var regString = "<widget([^>]+)*>(.*)<\\/widget>";
+
+//匹配标签的属性和值 k=v
+var prostr = /(\S+)\s*\=\s*("[^"]*")|('[^']*')/gi;
+
+// 获取属性对象
+function getPropsObj(props) {
+    var obj = {};
+
+    if (props) {
+        var propsArr = props.trim().match(prostr);
+
+        obj = require("querystring").parse(propsArr.join("&").replace(/["']/g, ""));
+
+    }
+
+    return obj;
+}
+
+//获取组件模板
+function getWidgetTemplate(id,file) {
+    if (!id) {
+        fis.log.error("未指定组件id");
+    }
+
+    var template = "";
+
+    var ret = id.split(":").reverse();
+
+    var project = fis.get("project");
+
+    //子系统，没指定为当前子系统
+    var stystem = ret[1] || project;
+
+    //组件名
+    var name = ret[0];
+
+    var widgetPath = 'widget/' + name + '/' + name + '.html';
+
+
+    //如果是本系统或者没指定子系统
+    if (stystem == project) {
+
+        // 通过该方式进行资源定位，绝对路径
+        template = '<link rel="import" href="/' + widgetPath + '?__inline">';
+
+
+        //跨系统
+    } else {
+        var version = require(projectPath+"/../common/package.json").version;
+        var media = fis.project.currentMedia() || "dev";
+
+        // 跨系统获取资源依赖表
+        var mapPath = projectPath+"/"+outputPath+"/" + media + "/map/" + stystem + "/" + version + "/map.json";
+
+
+        var map = require(mapPath);
+
+        var uri = map.res[stystem+":"+widgetPath].uri;
+
+
+
+        // file.addRequire(stystem+":"+widgetPath);
+        file.addAsyncRequire(stystem+":"+widgetPath);
+
+        template = fis.util.read(outputPath+"/"+media+"/template" + uri);
+    }
+
+
+    return template;
+
+}
+
+
+//获取组件列表
+
+function render(content, file) {
+
+    var pattern = new RegExp(regString, "gim");
+
+    var widgets = content.match(pattern);
+
+    if (widgets) {
+        content = content.replace(pattern, function(tag, props) {
+
+            var propsObj = getPropsObj(props);
+
+            var template = getWidgetTemplate(propsObj["id"],file);
+
+
+            tag = '<widget ' + props.trim() + '>'+template+'</widget>';
+
+
+            return tag;
+
+
+        });
+
+
+    }
+
+
+    return content;
+}
+
+module.exports = function(content, file, settings) {
+
+    var option = settings || {};
+
+
+
+   var content = render(content, file);
+
+   return content;
+}
