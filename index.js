d3e7b804b4da922c1bffc3c2449b8120dede08e0
guoxuemin <guo469477762@gmail.com>
2016-02-16 19:23:45 +0800

    抽取参数

--- index.js
+++ index.js
@@ -1,88 +1,20 @@
 var projectPath = fis.project.getProjectPath();
 
+var media = fis.project.currentMedia() || "dev";
+
 // 获取当前项目名称
-var currentProject = require(projectPath+'/package.json').name;
+var project = require(projectPath + '/package.json').name;
 
-// 输出路径
-var outputPath = "../../_output";
+var tagName, outputPath;
 
-//匹配组件标签<w-widget></w-widget>
-var regString = "<widget([^>]+)*>(.*)<\\/widget>";
+module.exports = function(content, file, conf) {
 
-//匹配标签的属性和值 k=v
-var prostr = /(\S+)\s*\=\s*("[^"]*")|('[^']*')/gi;
+    tagName = conf.tagName || "widget";
 
-// 获取属性对象
-function getPropsObj(props) {
-    var obj = {};
+    outputPath = conf.outputPath || "../../outputPath";
 
-    if (props) {
-        var propsArr = props.trim().match(prostr);
-
-        obj = require("querystring").parse(propsArr.join("&").replace(/["']/g, ""));
-
-    }
-
-    return obj;
-}
-
-//获取组件模板
-function getWidgetTemplate(id,file) {
-    if (!id) {
-        fis.log.error("未指定组件id");
-    }
-
-    var template = "";
-
-    var ret = id.split(":").reverse();
-
-
-    //子系统，没指定为当前子系统
-    var project = ret[1] || currentProject;
-
-    //组件名
-    var name = ret[0];
-
-    var widgetPath = 'widget/' + name + '/' + name + '.html';
-
-
-    //如果是本系统或者没指定子系统
-    if (project == currentProject) {
-
-        // 通过该方式进行资源定位，绝对路径
-        template = '<link rel="import" href="/' + widgetPath + '?__inline">';
-
-
-        //跨系统
-    } else {
-        var version = require(projectPath+"/../"+project+"/package.json").version;
-        var media = fis.project.currentMedia() || "dev";
-
-        // 跨系统获取资源依赖表
-        var mapPath = projectPath+"/"+outputPath+"/" + media + "/map/" + project + "/" + version + "/map.json";
-
-
-        var map = require(mapPath);
-
-        var uri = map.res[project+":"+widgetPath].uri;
-
-
-
-        file.addRequire(project+":"+widgetPath);
-        // file.addAsyncRequire(project+":"+widgetPath);
-
-        template = fis.util.read(outputPath+"/"+media+"/template" + uri);
-    }
-
-
-    return template;
-
-}
-
-
-//获取组件列表
-
-function render(content, file) {
+    //匹配组件标签<w-widget></w-widget>
+    var regString = "<" + tagName + "([^>]+)*>(.*)<\\/" + tagName + ">";
 
     var pattern = new RegExp(regString, "gim");
 
@@ -93,11 +25,9 @@ function render(content, file) {
 
             var propsObj = getPropsObj(props);
 
-            var template = getWidgetTemplate(propsObj["id"],file);
+            var template = getWidgetTemplate(propsObj["id"], file);
 
-
-            tag = '<widget ' + props.trim() + '>'+template+'</widget>';
-
+            tag = tag.replace(/>.*<\//, ">" + template + "</");
 
             return tag;
 
@@ -111,13 +41,75 @@ function render(content, file) {
     return content;
 }
 
-module.exports = function(content, file, settings) {
-
-    var option = settings || {};
 
 
 
-   var content = render(content, file);
+//获取组件模板
+function getWidgetTemplate(id, file) {
 
-   return content;
+    if (!id) {
+        fis.log.error("未指定组件id");
+    }
+
+    var template = "";
+
+    var ids = id.split(":").reverse();
+
+
+    //子系统，没指定为当前子系统
+    var namespace = ids[1] || project;
+
+    //组件名
+    var name = ids[0];
+
+    var widgetPath = tagName + '/' + name + '/' + name + '.html';
+
+
+    //如果是本系统或者没指定子系统
+    if (namespace == project) {
+
+        // 通过该方式进行资源定位，绝对路径
+        template = '<link rel="import" href="/' + widgetPath + '?__inline">';
+
+
+        //跨系统
+    } else {
+        var version = require(projectPath + "/../" + namespace + "/package.json").version;
+
+
+        // 跨系统获取资源依赖表
+        var mapPath = projectPath + "/" + outputPath + "/" + media + "/map/" + namespace + "/" + version + "/map.json";
+
+        var map = require(mapPath);
+
+        var uri = map.res[namespace + ":" + widgetPath].uri;
+
+        file.addRequire(namespace + ":" + widgetPath);
+
+        template = fis.util.read(outputPath + "/" + media + "/template" + uri);
+    }
+
+
+    return template;
+
+}
+
+
+
+//匹配标签的属性和值 k=v
+var prostr = /(\S+)\s*\=\s*("[^"]*")|('[^']*')/gi;
+
+// 获取属性对象
+function getPropsObj(props) {
+    var obj = {};
+
+    if (props) {
+        var propsArr = props.trim().match(prostr);
+
+
+        obj = require("querystring").parse(propsArr.join("&").replace(/["']/g, ""));
+
+    }
+
+    return obj;
 }
